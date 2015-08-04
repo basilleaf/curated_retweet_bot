7f68ba29e2ab24b900e8de1b207b7f0747002b32
Lisa Ballard <lballard.cat@gmail.com>
2015-08-03 22:00:31 -0700

    now it's just a lil twitter retweet bot okay

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 a bot that retweets my favorites, to be run on cron 
+
+for @hellohiking
--- retweet.py
+++ retweet.py
@@ -1,23 +1,44 @@
 import tweepy
-import json
-import redis 
-import random
-from auth import requires_auth
-from time import strftime
-from config import queue_max_rows
+from random import randint
+from time import sleep
 from secrets import consumer_key, consumer_secret, access_token, access_token_secret
 
-rs = redis.Redis("localhost")
+base_path = '/users/lballard/projects/hellohiking/'
+tweeted_file = base_path + 'tweeted.txt'
 
-def get_next_tweet():
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+def get_previously_tweeted():
+
+    # get previously tweeted
+    with open(tweeted_file) as f:
+        tweeted = f.read().splitlines()
+
+    return tweeted
+
+
+def get_next_tweet(page=None):
     """ get next tweet in queue, returns twitter tweet id """
-    try:
-        all_tweets = json.loads(rs.get('tweets'))
-    except TypeError: 
-        all_tweets = []
+        
+    if not page:
+        page = 1
 
-    tweet_id = random.choice([i['tweet_id'] for i in all_tweets if not i['published']])
-    return tweet_id
+    tweeted = get_previously_tweeted()
+
+    tweepy_favs = api.favorites(page=page)  # first page of favs = 20 favs
+    tweepy_favs.reverse()
+
+    for tweet_id in [f.id for f in tweepy_favs]:
+        if str(tweet_id) not in tweeted:
+            return tweet_id
+
+    # if we are still here then we didn't find a tweet in the first page of favs
+    # that has not already been tweeted, so go to next page
+    page = page + 1
+    return get_next_tweet(page=page)
 
 
 def retweet(tweet_id):
@@ -25,40 +46,33 @@ def retweet(tweet_id):
 
     tweet_id = int(tweet_id)
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    api = tweepy.API(auth)
     retweeted = True
     try:
         api.retweet(id=tweet_id)
+        mark_as_retweeted(tweet_id)
+        print "ok %s" % tweet_id
     except tweepy.error.TweepError:
         print tweepy.error.TweepError.__str__
-        retweeted = False
-
-    mark_as_retweeted(tweet_id, retweeted)
-
-    if not retweeted:
-        print "unable to tweet, marked published=error"
-    else:
-        print "ok %s" % tweet_id
+            
+    mark_as_retweeted(tweet_id)  # mark as retweeted either way
 
 
-def mark_as_retweeted(tweet_id, retweeted):
-    """ updates the tweet's published field """
-    # now set its published date and update the database
-    try:
-        all_tweets = json.loads(rs.get('tweets'))
-    except TypeError: 
-        all_tweets = []
+def mark_as_retweeted(tweet_id):
+    """ writes the tweet id to the file """
+    print 'wriing to file'
+    with open (tweeted_file, 'a') as f: 
+        f.write ("%i\n" % tweet_id)
 
-    index = [i['tweet_id'] for i in all_tweets].index(tweet_id)
-    all_tweets[index]['published'] = strftime("%Y-%m-%d %H:%M:%S")
-    if not retweeted:
-        all_tweets[index]['published'] = 'error'
-    rs.set('tweets', json.dumps(all_tweets[0:queue_max_rows]))
 
 
 if __name__ == '__main__':
+    # this will be on something like hourly cron, 
+    # but we don't want to really tweet on the hour 
+    # or even at the same time every day
+    # so sleep for a random number of seconds between zero and 1 hour 
+    sleep_time = randint(0,3600)
+    print "sleeping for %s" % sleep_time
+    sleep(sleep_time)
     tweet_id = get_next_tweet()
     retweet(tweet_id)
+
