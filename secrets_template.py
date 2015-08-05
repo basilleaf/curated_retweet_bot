0b5e6e1dadea743bd27534c521b6a37546f5f1a3
Lisa Ballard <basilleaf@users.noreply.github.com>
2015-08-05 11:24:04 -0700

    Update secrets_template.py

--- secrets_template.py
+++ secrets_template.py
@@ -1,9 +1,5 @@
 # twitter auth creds
 consumer_key = ''  
 consumer_secret = ''
-uaccess_token = ''
-uaccess_token_secret = ''
-
-# your flask app's user and pw
-app_user = '' 
-app_pw = ''
+access_token = ''
+access_token_secret = ''
